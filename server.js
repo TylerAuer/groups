0d31be83368b75f3e2146b442107255ce647ded4
Tyler Auer <Tyler.Auer@gmail.com>
2020-10-24 13:38:28 -0700

    add session store to DB, change passwords after uploading .env

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
 const express = require('express');
+const session = require('express-session');
+const Sequelize = require('sequelize');
 const secure = require('express-force-https');
 const chalk = require('chalk');
 const db = require('./models');
 const { loginUser } = require('./backend/users/loginUser');
 require('dotenv').config();
 
+// Create store for session data in Postgres DB
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sessionStore = new SequelizeStore({
+  db: db.sequelize,
+  tableName: 'GroupUsSessions',
+});
+
+db.sequelize.sync();
+
 const app = express();
 app.use(secure);
 app.use(express.json());
-
-db.sequelize.sync();
+app.use(
+  session({
+    secret: process.env.COOKIE_SESSIONS_SECRET,
+    store: sessionStore,
+    secure: false,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 40 days
+    },
+  })
+);
 
 const port = process.env.PORT || 4000;
 
@@ -24,14 +45,15 @@ app.get('/', (req, res) => res.sendFile(__dirname + '/build/index.html'));
 // Account
 app.post('/api/login', loginUser);
 // app.get('/api/logout')
+// app.get('/api/disconnect)
 
 ////////////////////////////////////
 // SECTION API
 
 // app.get('/api/section/:id')
-// app.post('/api/section')
-// app.put('/api/section')
-// app.delete('/api/section')
+// app.post('/api/section/:id')
+// app.put('/api/section/:id')
+// app.delete('/api/section/:id')
 
 app.listen(port, () =>
   console.log(
